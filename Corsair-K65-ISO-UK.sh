bf3bc0d3e9ef24e153bf5d02e7a1b24e9082ff60
Richard Dawe <richdawe@gmail.com>
2020-04-06 17:02:14 +0100

    Working key mapping script for K65 ISO UK

---
+++ Corsair-K65-ISO-UK.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Needs keyboard set to "British - PC" to work
+
+# USB usage tables (from the USB HID Usage Tables (HUT) doc):
+#  1 (0x01) = Generic desktop page
+# 12 (0x0c) = Consumer page
+
+# hidutil list for keyboard:
+#
+# Services:
+# VendorID ProductID LocationID         UsagePage Usage RegistryID  Transport Class                         Product                            UserClass               Built-In 
+# 0x1b1c   0x1b07    0x14110000         1         6     0x1000007aa USB       AppleUserHIDEventService      Corsair K65 Gaming Keyboard        AppleUserHIDEventDriver (null)   
+# 0x1b1c   0x1b07    0x14110000         12        1     0x100000783 USB       AppleUserHIDEventService      Corsair K65 Gaming Keyboard        AppleUserHIDEventDriver (null)   
+# 0x1b1c   0x1b07    0x14110000         1         6     0x10000074b USB       AppleUserHIDEventService      Corsair K65 Gaming Keyboard        AppleUserHIDEventDriver (null)   
+# Devices:
+# VendorID ProductID LocationID         UsagePage Usage RegistryID  Transport Class                    Product                             UserClass                 Built-In 
+# 0x1b1c   0x1b07    0x14110000         1         6     0x100000707 USB       AppleUserHIDDevice       Corsair K65 Gaming Keyboard         AppleUserUSBHostHIDDevice 0        
+# 0x1b1c   0x1b07    0x14110000         12        1     0x100000719 USB       AppleUserHIDDevice       Corsair K65 Gaming Keyboard         AppleUserUSBHostHIDDevice 0        
+# 0x1b1c   0x1b07    0x14110000         1         6     0x100000724 USB       AppleUserHIDDevice       Corsair K65 Gaming Keyboard         AppleUserUSBHostHIDDevice 0        
+# Default mappings (none):
+#
+# RegistryID  Key                   Value
+# 1000007aa   UserKeyMapping   (null)
+# 100000783   UserKeyMapping   (null)
+# 10000074b   UserKeyMapping   (null)
+
+# VendorID ProductID ... Product
+# 0x1b1c   0x1b07    ... Corsair K65 Gaming Keyboard
+MATCH='{"ProductID":0x1b07,"VendorID":0x1b1c}'
+
+echo "Old mapping:"
+sudo hidutil property --matching "$MATCH" --get "UserKeyMapping"
+
+echo
+echo "Clearing mapping:"
+sudo hidutil property --matching "$MATCH" --set '{"UserKeyMapping":[]}'
+#sudo hidutil property --set '{"UserKeyMapping":[]}'
+
+# XXX: mappings?
+MAPPING=$(cat <<EOT | grep -v '^#'
+{
+# Swap "non-US \ and |" with "grave accent and tilde"
+# to make sure they are on the right keys
+# (assuming "British - PC" Input Source has been selected).
+  "UserKeyMapping": [
+    {
+      "HIDKeyboardModifierMappingSrc": 0x700000064,
+      "HIDKeyboardModifierMappingDst": 0x700000035
+    },
+    {
+      "HIDKeyboardModifierMappingSrc": 0x700000035,
+      "HIDKeyboardModifierMappingDst": 0x700000064
+    },
+# Map Application key (context menu key) to Left GUI,
+# which is then mapped by OS to Option
+# (assuming that Windows and Alt keys have been swapped
+# to Option and Command).
+    {
+      "HIDKeyboardModifierMappingSrc": 0x700000065,
+      "HIDKeyboardModifierMappingDst": 0x7000000E2
+    }
+  ]
+}
+EOT
+)
+
+#    {
+#      "HIDKeyboardModifierMappingSrc": 0x7000000E7,
+#      "HIDKeyboardModifierMappingDst": 0x7000000E3
+#    }
+
+#    {
+#     "HIDKeyboardModifierMappingSrc": 0x700000031,
+#     "HIDKeyboardModifierMappingDst": 0x700000032
+#   }
+#    {
+#     "HIDKeyboardModifierMappingSrc": 0x700000031,
+#      "HIDKeyboardModifierMappingDst": 0x700000064
+#    }
+#  ]
+#}
+#EOT
+#)
+
+SHORTMAPPING=$(echo "$MAPPING" | perl -ne 's/\s//g; print;')
+#echo "$MAPPING"
+#echo "$SHORTMAPPING"
+
+#exit 0
+
+echo
+echo "Setting mapping:"
+sudo hidutil property --matching "$MATCH" --set "$SHORTMAPPING"
+#sudo hidutil property --set "$SHORTMAPPING"
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # mac-keyboards
 Scripts for Keyboards for macOS
+
+## Corsair K65 Keyboard (ISO UK)
+
+Name: Vengeance K65 Compact Mechanical Gaming Keyboard – (UK English)
+Part Number: CH-9000040-UK
+
+The script here makes it possible to use the K65 with UK layout under macOS Catalina. It may work with earlier releases, but I have only tested it on macOS Catalina. It definitely requires at least macOS Sierra (10.12) for the hidutil program.
+
+Using a combination of macOS settings and the script, it's possible to use all the ISO UK keys and still benefit from special macOS key combinations like `Option + e e` to get an e-acute.
+
+### Requirements
+
+- Swap Option and Command, so that the Windows key maps to Option, and Alt maps to Command. To do that, open the "System Preferences" app, then open "Keyboard", then open `Modifier Keys...`, select the keyboard in the drop-down box, then swap the keys in the other drop-down boxes.
+
+- Add "British - PC" to the list of input sources. To do that, open the "System Preferences" app, then open "Keyboard", then "Input Sources". Click on the plus to add `British - PC`, and then move it to the top to activate it. You should also enable the Input Sources icon in the menu bar, to make it easier to switch to/from different layouts later.
+
+### Usage
+
+```
+./Corsair-K65-ISO-UK.sh
+```
+
+### Notes
+
+- Switch input source using `Ctrl + Space` (brings up menu) or `Ctrl + Option (Windows) + Space` to cycle through them.
+
+- The right Application key (context menu key) is mapped to the Windows key, which means it's effectively the Option key.
+
+- Media keys `Fn + F10-F12` work out of the box on macOS Catalina (previous, play/pause, next).
+
+- Special volume media keys at top (mute, volume up/down) work out of the box on macOS Catalina.
+
+### Known Problems
+
+- The `Command + \` shortcut for 1Password doesn't work. It seems like that's mapped at a lower level than the mappings set up by the script. Instead, use `Command + #` on the keyboard. (Note `Command` = `Alt` on the actual keys.)
+
+- F-keys don't work because there are some pre-existing shortcuts by macOS set up that bypass the F-keys. It's possible to disable this behaviour, by disabling the shortcuts.
+
+- `Home/End/etc.` keys don't work.
+
+- How to have this applied during Mac startup? (Currently it's lost across restarts.)
+
+- `Fn + F9` doesn't work right now (stop media key) -- why is that?
+
+- Windows lock button doesn't seem to do anything on macOS Catalina.
